4a5351bfe78b9e688e3c319d21e431ced01a99c7
Apll1211 <z490135438@gmail.com>
2025-11-14 21:06:13 +0800

    嵌入框架

--- my-electron-app/main.js
+++
@@ -1,9 +0,0 @@
-const { app, BrowserWindow } = require("electron");
-
-app.whenReady().then(() => {
-	const win = new BrowserWindow({
-		width: 800,
-		height: 600,
-	});
-	win.loadFile("./home/index.html");
-});
